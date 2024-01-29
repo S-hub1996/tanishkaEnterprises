5287aa3ff2befbd6c93d0d502a7523ea3bf588ad
shubham verma <sv7836832607@gmail.com>
2024-01-29 18:31:03 +0530

    content drop added

--- my-app/src/components/Tesstimonial.jsx
+++ my-app/src/components/Tesstimonial.jsx
@@ -1,80 +1,80 @@
 'use client'
 
 import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  Flex,
-  useColorModeValue,
-  Text,
-  Container,
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    Flex,
+    useColorModeValue,
+    Text,
+    Container,
 } from '@chakra-ui/react'
 
 import { ChevronDownIcon } from '@chakra-ui/icons'
 
 export default function Testimonial() {
-  return (
-    <Flex
-    //   minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Container>
-        <Accordion allowMultiple width="100%" maxW="lg" rounded="lg">
-          <AccordionItem>
-            <AccordionButton
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              p={4}>
-              <Text fontSize="md">What is Chakra UI?</Text>
-              <ChevronDownIcon fontSize="24px" />
-            </AccordionButton>
-            <AccordionPanel pb={4}>
-              <Text color="gray.600">
-                Chakra UI is a simple and modular component library that gives developers
-                the building blocks they need to create web applications.
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionButton
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              p={4}>
-              <Text fontSize="md">What advantages to use?</Text>
-              <ChevronDownIcon fontSize="24px" />
-            </AccordionButton>
-            <AccordionPanel pb={4}>
-              <Text color="gray.600">
-                Chakra UI offers a variety of advantages including ease of use,
-                accessibility, and customization options. It also provides a comprehensive
-                set of UI components and is fully compatible with React.
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionButton
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              p={4}>
-              <Text fontSize="md">How to start using Chakra UI?</Text>
-              <ChevronDownIcon fontSize="24px" />
-            </AccordionButton>
-            <AccordionPanel pb={4}>
-              <Text color="gray.600">
-                To get started with Chakra UI, you can install it via npm or yarn, and
-                then import the components you need in your project. The Chakra UI
-                documentation is also a great resource for getting started and learning
-                more about the library.
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      </Container>
-    </Flex>
-  )
+    return (
+        <Flex
+            //   minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}>
+            <Container>
+                <Accordion allowMultiple width="100%" maxW="lg" rounded="lg">
+                    <AccordionItem>
+                        <AccordionButton
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="space-between"
+                            p={4}>
+                            <Text fontSize="md">What Tanishka Enterprises Ltd. is?</Text>
+                            <ChevronDownIcon fontSize="24px" />
+                        </AccordionButton>
+                        <AccordionPanel pb={4}>
+                            <Text color="gray.600">
+                                Your Premier Destination for Quality Sports Wear Wholesale and Manufacturing in New Delhi
+
+                                At Tanishka Enterprises Ltd., we pride ourselves on being the leading wholesaler and manufacturer of premium sports wear for men and women in New Delhi. With a commitment to quality, style, and performance, we cater to the diverse needs of athletes, fitness enthusiasts, and sports teams nationwide.
+                            </Text>
+                        </AccordionPanel>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <AccordionButton
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="space-between"
+                            p={4}>
+                            <Text fontSize="md">Why Choose Tanishka Enterprises Ltd.?</Text>
+                            <ChevronDownIcon fontSize="24px" />
+                        </AccordionButton>
+                        <AccordionPanel pb={4}>
+                            <Text color="gray.600">
+                                Quality Assurance: We adhere to stringent quality standards, ensuring that each product meets our rigorous specifications.
+                                Innovative Designs: Our team of designers stays ahead of the curve, bringing you the latest trends and innovations in sports wear.
+                                Customization Options: We understand that every client has unique needs. That's why we offer customization services to tailor our products according to your preferences.
+                                Competitive Pricing: Enjoy competitive wholesale prices without compromising on quality, making Tanishka Enterprises Ltd. the go-to choice for businesses and retailers.
+                                Exceptional Customer Service: Our dedicated team is committed to providing prompt assistance and personalized support to ensure a seamless experience from order placement to delivery.
+                            </Text>
+                        </AccordionPanel>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <AccordionButton
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="space-between"
+                            p={4}>
+                            <Text fontSize="md">Partner with Us</Text>
+                            <ChevronDownIcon fontSize="24px" />
+                        </AccordionButton>
+                        <AccordionPanel pb={4}>
+                            <Text color="gray.600">
+                            Whether you're a retailer looking to stock premium sports wear or a sports team in need of custom uniforms, Tanishka Enterprises Ltd. is your reliable partner. With flexible ordering options, timely delivery, and unparalleled expertise, we make collaboration easy and rewarding.
+                            </Text>
+                        </AccordionPanel>
+                    </AccordionItem>
+                </Accordion>
+            </Container>
+        </Flex>
+    )
 }
