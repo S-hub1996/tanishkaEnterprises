40579b5c872110e2fac179847fcdac3cc72bf748
shubham verma <sv7836832607@gmail.com>
2024-01-31 17:23:21 +0530

    marquee edited 3

--- my-app/src/components/Marquee.jsx
+++ my-app/src/components/Marquee.jsx
@@ -9,7 +9,7 @@ export default function MarqueeSlider(){
 
  return <Box position={'fixed'} w={'full'}   zIndex={4} height="50px"  textAlign={'center'}>
   <Marquee >
-    <Code  color={"gray.100"} bg={'gray.900'} fontSize={['lg','2xl']}>| Tanishka Enterprises || Sports Wear | Manufacturing | Whole Selling | Product Designing | Custom Branding | Impoted Fabric Material ||</Code>
+    <Code  color={"gray.100"} bg={'gray.900'} fontSize={['lg','2xl']}>| Tanishka Enterprises || Sports Wear | Manufacturing | Bulk Whole Selling | Product Designing | Custom Branding | Impoted Fabric Material ||</Code>
   </Marquee>
 </Box>
 
